e6a5cd6baa09eddf5ff8ded3c5fecc5f9e94eb14
rafael-vitor <rafael.vitor@jusbrasil.com.br>
2020-03-11 00:26:19 -0300

    Vendors file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
 import { run } from './app';
 
 import './main.scss';
 
+console.log(_.join(['um ', 'dois']));
 
 run();
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,11 +5,22 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = merge(common, {
   output: {
-    filename: 'main.[contentHash].js',
+    filename: '[name].[contentHash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin(),
   ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        }
+      }
+    }
+  }
 });
